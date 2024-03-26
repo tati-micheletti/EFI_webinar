e87e9aa7c0a562a9db538c7b353492dff6a2a6a8
Tati Micheletti <tati.micheletti@gmail.com>
2024-03-26 19:37:16 +0100

    ToDo for Presentation Still

---
+++ todo.R
@@ -0,0 +1,33 @@
+## THINGS TO DO:
+# [X] Add something in the form of outputs (saving the raster stack and plots?) (both files!!)
+# <!-- C:\Users\Tati\Documents\GitHub\integratingSpaDESmodules\modules\speciesAbundance == pulling / testing -->
+# <!-- C:\Users\Tati\Documents\GitHub\speciesAbundance == pushing -->
+# [X] Add change plotting time of the module (change parameter)
+# [X] Fix temperature data. Identical after 2022?
+# [X] Prepare 2 modules: Temperature 
+# [X] and LM with forecasting
+# [X] Add a project script about mix matching modules: first abundance alone, then with Temperature, then with LM, at 
+# last with forecasting
+# [X] Commit and push all changes
+
+# WEDNESDAY:
+# [X] Versioning
+# [ ] Come up with more interesting data!!!
+# [ ] Publishing all the modules in Zenodo: 1/3
+# [ ] Publishing all the modules in Zenodo: 2/3
+# [ ] Publishing all the modules in Zenodo: 3/3
+# [ ] Push the whole Webinar to GitHub
+# [ ] Publish the whole Webinar in Zenodo
+# [ ] Work on README files
+# [ ] Work on Module's manuals
+# [ ] Add citations
+# [ ] Add tags to the versions of SpaDES.project, Require, SpaDES.core, reproducible for the installation process
+# [ ] Add figures of results to rmd to make it more appealing!
+# [ ] Test in other machines
+# [ ] Ask for help to write tests? 
+
+# Checklist Upload to Statistical Series:
+# [ ] README
+# [ ] HandsOn Html and PDF
+# [ ] Presentation in PDF
+# [ ] Data (update before putting up!)
