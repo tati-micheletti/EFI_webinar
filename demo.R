60a81c7d80bb660c7e718c0c359db007ed2e47ef
Tati Micheletti <tati.micheletti@gmail.com>
2024-03-27 16:23:07 +0100

    Demo added + Update todo

---
+++ demo.R
@@ -0,0 +1,41 @@
+# Before Starting:
+# 1. Install/Update R: 4.3.2
+# 2. Make sure R development tools are installed:
+#   Windows: install Rtools as administrator. (https://cran.r-project.org/bin/windows/Rtools/rtools43/rtools.html)
+#   macOS: install Xcode commandline tools from the terminal: xcode-select --install.
+#   Debian/Ubuntu Linux: ensure r-base-dev is installed.  
+# 3. Run the code:
+wd <- "~/SpaDES_Projects_demo"
+lib <- file.path(wd, "library")
+dir.create(wd)
+dir.create(lib)
+setwd(wd)
+.libPaths(lib)
+.libPaths()
+
+install.packages("devtools", lib = lib) # You might be prompted to reinstall packages from source. Please allow it.  
+
+
+devtools::install_github("PredictiveEcology/Require", ref = "3b239d6d4d18fe39dfa40a730df5094e74c086f8")
+devtools::install_github("PredictiveEcology/reproducible", ref = "40033e6ddea151cbd31021ad73013393d71e1f06")
+devtools::install_github("PredictiveEcology/SpaDES.core", ref = "a36dac8ecfc29173294b1bf76fbc63fa1d60e122")
+devtools::install_github("PredictiveEcology/SpaDES.project", ref = "e32abe20d89f97d03996b4335655ad000dbab89b")
+
+
+runName <- "integratedDefault"
+out <- SpaDES.project::setupProject(
+  runName = runName,
+  paths = list(projectPath = "integratingSpaDESmodules",
+               modulePath = file.path(dirname(wd), "SpaDES_Modules"),
+               outputPath = file.path("outputs", runName)),
+  modules = c("tati-micheletti/speciesAbundance@main",
+              "tati-micheletti/temperature@main",
+              "tati-micheletti/speciesAbundTempLM@main"),
+  times = list(start = 2013,
+               end = 2032),
+  loadOrder = c("speciesAbundance",
+                "temperature",
+                "speciesAbundTempLM"),
+  updateRprofile = FALSE)
+
+snippsim <- do.call(SpaDES.core::simInitAndSpades, out)
--- todo.R
+++ todo.R
@@ -15,6 +15,12 @@
 # [X] Come up with more interesting data!!!
 # [X] Push the whole Webinar to GitHub
 # [X] Work on README files
+# [ ] Versioning of SpaDES.core: "2.0.3", "a36dac8ecfc29173294b1bf76fbc63fa1d60e122" 
+# [ ] Versioning of reproducible: "2.0.10", "40033e6ddea151cbd31021ad73013393d71e1f06"
+# [ ] Versioning of Require: "0.3.1.9044", "3b239d6d4d18fe39dfa40a730df5094e74c086f8"
+# [ ] Versioning of SpaDES.project: "0.0.8.9040", "e32abe20d89f97d03996b4335655ad000dbab89b"
+# [ ] Work on Bullet points direct file
+# [ ] Add minimum R Version: 4.3.2
 # [ ] Work on Module's manuals
 # [ ] Add citations
 # [ ] Add tags to the versions of SpaDES.project, Require, SpaDES.core, reproducible for the installation process
